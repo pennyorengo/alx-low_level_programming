8e3012303478026ea65b812c9745cefc4d7a7f36
Penny Orengo <pennyorengo3@gmail.com>
2022-09-22 12:17:30 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,79 +1,41 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
- *
- * Return: address of r or 0
- */
+int main(void)
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
 
-	for (i = 0; n1[i]; i++)
+  int n;
 
-		;
+  int a[5];
 
-	for (j = 0; n2[j]; j++)
+  int *p;
 
-		;
+  a[2] = 1024;
 
-	if (i > size_r || j > size_r)
+  p = &n;
 
-		return (0);
+  /*
 
-	m = 0;
+   * write your line of code here...
 
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+   * Remember:
 
-	{
+   * - you are not allowed to use a
 
-		n = m;
+   * - you are not allowed to modify p
 
-		if (i >= 0)
+   * - only one statement
 
-			n += n1[i] - '0';
+   * - you are not allowed to code anything else than this line of code
 
-		if (j >= 0)
+   */
 
-			n += n2[j] - '0';
+  *(p + 5) = 98;
 
-		if (i < 0 && j < 0 && n == 0)
+  /* ...so that this prints 98\n */
 
-		{
+  printf("a[2] = %d\n", a[2]);
 
-			break;
-
-		}
-
-		m = n / 10;
-
-		r[k] = n % 10 + '0';
-
-	}
-
-	r[k] = '\0';
-
-	if (i >= 0 || j >= 0 || m)
-
-		return (0);
-
-	for (k -= 1, l = 0; l < k; k--, l++)
-
-	{
-
-		m = r[k];
-
-		r[k] = r[l];
-
-		r[l] = m;
-
-	}
-
-	return (r);
+  return (0);
 
 }
