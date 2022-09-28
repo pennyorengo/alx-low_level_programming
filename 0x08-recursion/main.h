0a588aa091ecfca8b55c2ad45532964afebe4880
Penny Orengo <pennyorengo3@gmail.com>
2022-09-28 12:52:31 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 /**
  * void prototypes()
  * int prototypes()
@@ -15,6 +17,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-_putchar()
+int _putchar(char c)
 
 #endif /*MAIN_H*/
