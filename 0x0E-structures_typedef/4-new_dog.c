e1b033addb639d741da2c58dd31b704cfbd33045
Penny Orengo <pennyorengo3@gmail.com>
2022-10-11 12:34:11 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,29 +1,18 @@
 #include <stdlib.h>
-
 #include "dog.h"
 
-
-
 /**
-
  * new_dog - creates a new dog
-
  * @name: name of dog
-
  * @age: age of dog
-
  * @owner: owner of dog
-
  *
-
  * Return: pointer to new dog
-
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 
 {
-
 	unsigned int nl, ol, i;
 
 	dog_t *dog;
@@ -91,4 +80,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 	return (dog);
 
 }
-
