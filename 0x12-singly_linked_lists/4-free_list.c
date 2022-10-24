0adbcf2020d49345ab897ca51dfc36e082543365
Penny <109464029+pennyorengo@users.noreply.github.com>
2022-10-25 01:18:15 +0300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,22 +1,14 @@
 #include "lists.h"
 
-
-
 /**
-
  *free_listint - A function that frees list.
-
  * @head: head.
-
  */
 
-void free_listint(listint_t *head)
-
+void free_list(list_t *head)
 {
 
-	listint_t *liberador;
-
-
+	list_t *liberador;
 
 	while (head != NULL)
 
