5b8490d305f552b8ccacd6ab01fa835e1cb27b7c
Penny <109464029+pennyorengo@users.noreply.github.com>
2022-10-25 01:08:34 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,74 +1,60 @@
 #ifndef LISTS_H
-
 #define LISTS_H
 
-
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
 
-
-
 /**
-
  * struct listint_s - singly linked list
-
  * @n: integer
-
  * @next: points to the next node
-
  *
-
  * Description: singly linked list node structure
-
  * 
-
  */
 
-typedef struct listint_s
+typedef struct list_s
 
 {
 
     int n;
 
-    struct listint_s *next;
+    struct list_s *next;
 
-} listint_t;
+} list_t;
 
 int _putchar(char c);
 
-size_t print_listint(const listint_t *h);
+size_t print_list(const listint_t *h);
 
-size_t listint_len(const listint_t *h);
+size_t list_len(const listint_t *h);
 
-listint_t *add_nodeint(listint_t **head, const int n);
+list_t *add_node(list_t **head, const int n);
 
-listint_t *add_nodeint_end(listint_t **head, const int n);
+list_t *add_node_end(list_t **head, const int n);
 
-void free_listint(listint_t *head);
+void free_list(list_t *head);
 
-void free_listint2(listint_t **head);
+void free_list2(list_t **head);
 
-int pop_listint(listint_t **head);
+int pop_list(list_t **head);
 
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+list_t *get_node_at_index(listint_t *head, unsigned int index);
 
-int sum_listint(listint_t *head);
+int sum_list(list_t *head);
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+list_t *insert_node_at_index(list_t **head, unsigned int idx, int n);
 
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
+int delete_node_at_index(list_t **head, unsigned int index);
 
-listint_t *reverse_listint(listint_t **head);
+list_t *reverse_list(list_t **head);
 
-size_t print_listint_safe(const listint_t *head);
+size_t print_list_safe(const list_t *head);
 
-size_t free_listint_safe(listint_t **h);
+size_t free_list_safe(list_t **h);
 
-listint_t *find_listint_loop(listint_t *head);
+list_t *find_list_loop(list_t *head);
 
 
 
