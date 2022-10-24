256bdc5a852896c5e1bc8fdd9d3a8816ac811237
Penny <109464029+pennyorengo@users.noreply.github.com>
2022-10-25 00:50:54 +0300

    Update and rename 0-print_listint.c to 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
-
 #include "lists.h"
 
-
-
 /**
-
   * print_listint - ...
-
   * @h: The head of the linked list
-
   *
-
   * Return: The number of nodes of the linked list
-
   */
 
 size_t print_listint(const listint_t *h)
 
 {
-
 	int nodes = 0;
 
 
@@ -42,9 +33,6 @@ size_t print_listint(const listint_t *h)
 
 	}
 
-
-
 	return (nodes);
 
 }
-
