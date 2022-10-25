b14c699c67aa5689c8537bcc7375b9ea32b0662f
Penny <109464029+pennyorengo@users.noreply.github.com>
2022-10-25 22:24:51 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,56 +7,25 @@
 
 /**
  * struct list_s - singly linked list
- * @n: integer
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ * for Holberton project
  */
-
 typedef struct list_s
-
 {
-
-    int n;
-
-    struct list_s *next;
-
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
-
-size_t print_list(const listint_t *h);
-
-size_t list_len(const listint_t *h);
-
-list_t *add_node(list_t **head, const int n);
-
-list_t *add_node_end(list_t **head, const int n);
-
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(const char *str);
 
-void free_list2(list_t **head);
-
-int pop_list(list_t **head);
-
-list_t *get_node_at_index(listint_t *head, unsigned int index);
-
-int sum_list(list_t *head);
-
-list_t *insert_node_at_index(list_t **head, unsigned int idx, int n);
-
-int delete_node_at_index(list_t **head, unsigned int index);
-
-list_t *reverse_list(list_t **head);
-
-size_t print_list_safe(const list_t *head);
-
-size_t free_list_safe(list_t **h);
-
-list_t *find_list_loop(list_t *head);
-
-
-
-#endif
-
+#endif /*LISTS_H*/
