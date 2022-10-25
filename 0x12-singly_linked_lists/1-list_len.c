16a67ad738b0bdb03c798251b3dbfda25f661b48
Penny <109464029+pennyorengo@users.noreply.github.com>
2022-10-25 22:14:38 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,28 +1,18 @@
 #include "lists.h"
 
 /**
- * list_len - a function that returns
- * the number of elements in a linked listint_t list.
- * @h: list.
- * Return: The elements linked.
+ * list_len - finds the number of elements in a linked list
+ * @h: pointer to the start of the linked list
+ *
+ * Return: number of elements in the linked list
  */
-
 size_t list_len(const list_t *h)
-
 {
+	const list_t *tmp;
+	unsigned int i;
 
-int i = 0;
-
-	while (h != NULL)
-
-	{
-
-		h = h->next;
-
-		i++;
-
-	}
-
+	tmp = h;
+	for (i = 0; tmp; i++)
+		tmp = tmp->next;
 	return (i);
-
 }
